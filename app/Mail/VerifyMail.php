a42a0717fcc31d9e4999ad303297c0ffc42f018f
B <henryirons@yahoo.com>
2021-09-22 16:38:47 +0100

    just finished VerifyMail.php

--- app/Mail/VerifyMail.php
+++ app/Mail/VerifyMail.php
@@ -6,19 +6,25 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
+use App\Models\User;
+use App\Models\Verification;
 
 class VerifyMail extends Mailable
 {
     use Queueable, SerializesModels;
+    protected $user;
+    protected $verification;
 
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(User $user,Verification $verification)
     {
         //
+        $this->user =$user;
+        $this->verification = $verification;
     }
 
     /**
@@ -28,6 +34,9 @@ class VerifyMail extends Mailable
      */
     public function build()
     {
-        return $this->view('view.name');
+        return $this->subject('welcome to our service, please continue the verification process')->view('mails.verify',[
+            'user'=> $this->user,
+            'verification'=>$this->verification
+        ]);
     }
 }
