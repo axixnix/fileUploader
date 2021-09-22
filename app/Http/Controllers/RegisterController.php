c2f1e681bb225cfa3a5ca0d0761f8f83fa7bfea5
B <henryirons@yahoo.com>
2021-09-22 16:20:39 +0100

    created the register controller wrote the logic for registering users

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -5,10 +5,11 @@ namespace App\Http\Controllers;
 use App\Mail\VerifyMail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
-use App\User;
-use App\Verification;
+use App\Models\User;
+use App\Models\Verification;
 use Illuminate\Support\Facades\Hash;
 use App\Utils\RandomFunctions;
+use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Support\Facades\Mail;
 
 class RegisterController extends Controller
@@ -25,7 +26,24 @@ class RegisterController extends Controller
             return response(['message'=>$validator->errors()->first()],422);
         }
 
-        $user = User::create([])
+        $user = User::create([
+            'name'=>'',
+            'email'=>$request->input('email'),
+            'password'=>Hash::make($request->input('password'))
+        ]);
+
+        $verification = new Verification;
+        $verification->payload = RandomFunctions::generateRandomString(5);
+        $check = true;
+        do{
+            $check = !is_null(Verification::where('payload',$verification->payload)->first());
+        }while($check);
+
+        $user->verifications()->save($verification);
+
+        Mail::to($user->email)->send(new VerifyMail($user,$verification));
+
+        return response(['message'=>'user created successfully'],200);
 
     }
 }
