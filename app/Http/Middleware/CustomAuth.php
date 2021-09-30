4c59dc17e866c238e485d18c36e59a24fc649a7b
B <henryirons@yahoo.com>
2021-09-30 11:24:49 +0100

    CustomAuth middleware done

---
+++ app/Http/Middleware/CustomAuth.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Contracts\Encryption\DecryptException;
+
+class CustomAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if($request->hasHeader('Authorization')){
+            try{
+                $header= $request->header('Authorization');
+                $value = Crypt::decryptString($header);
+                $user = User::find($value);
+
+                if($value && $user){
+                    $request->attributes->add(['user'=>$user]);
+                    Auth::onceUsingId($value);
+                    return $next($request);
+                }else{
+                    return response(['message'=>'unauthenticated'],401);
+                }
+            }catch(DecryptException $exp){
+                echo $exp->getMessage();
+                return response(['message'=>'unauthenticated'],401);
+            }
+        }
+        return response(['message'=>'unauthenticated'],401);
+    }
+}
