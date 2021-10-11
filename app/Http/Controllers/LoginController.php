be33812933dcd7420c16550f94ce81f41b41b716
B <henryirons@yahoo.com>
2021-10-11 13:26:16 +0100

    taken care of the error that was preventing registration

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -13,7 +13,10 @@ class LoginController extends Controller
 {
     //
     public function login(Request $request){
-        $user = User::Where('email',$request->input('identifier'))->orWhere('name',$request->input('identifier')->first());
+        //$user = User::Where('email',$request->input('identifier'))->orWhere('name',$request->input('identifier'))->first();
+        $user = User::where('email', $request->input('identifier'))
+            ->orWhere('name', $request->input('identifier'))
+            ->first();
         if(is_null($user)) return response(['message'=>'user credentials do not match our records'],401);
 
         if(Hash::check($request->input('password'), $user->password)){
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\LoginController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
